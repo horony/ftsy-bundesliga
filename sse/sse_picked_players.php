74ad449ff10a40d586c43ebb9ec0ffb08d3e089c
horony <81903255+horony@users.noreply.github.com>
2021-06-22 22:54:45 +0200

    Update sse_picked_players.php

--- sse/sse_picked_players.php
+++ sse/sse_picked_players.php
@@ -9,18 +9,22 @@ include '../db.php';
 $draft_status = mysqli_query($con, "SELECT draft_status FROM xa7580_db1.draft_meta WHERE league_id = 1 ") -> fetch_object() -> draft_status;
 
 $sql_result = mysqli_query($con, "	SELECT 		dof.round
-												, dof.pick
-												, dof.teamname
-												, dof.user_id
-												, dpb.common_name
-												, dpb.lastname
-												, case when dpb.pick is null then '/images/icons/questionmarks.png' else dpb.image_path end as image_path
+							, dof.pick
+							, dof.teamname
+							, dof.user_id
+							, dpb.common_name
+							, dpb.lastname
+							, case 	when dpb.pick is null then '/images/icons/questionmarks.png' 
+								else dpb.image_path 
+								end as image_path
 
-									FROM 		xa7580_db1.draft_order_full dof
-									LEFT JOIN	xa7580_db1.draft_player_base dpb
-										ON dof.pick = dpb.pick
-									WHERE 		dof.pick BETWEEN (SELECT current_pick_no-5 FROM xa7580_db1.draft_meta) AND (SELECT current_pick_no+4 FROM xa7580_db1.draft_meta)
-									ORDER BY 	dof.pick ASC
+					FROM 		xa7580_db1.draft_order_full dof
+					
+					LEFT JOIN	xa7580_db1.draft_player_base dpb
+						ON dof.pick = dpb.pick
+					WHERE 		dof.pick BETWEEN (SELECT current_pick_no-5 FROM xa7580_db1.draft_meta) 
+							AND (SELECT current_pick_no+4 FROM xa7580_db1.draft_meta)
+					ORDER BY 	dof.pick ASC
 									");
 
 $round_array = array(); 
