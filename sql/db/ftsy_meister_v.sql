11d9b1256333b8eee3253d27c795b8d00ac17ab5
Lennart <lhorony@gmail.com>
2023-03-13 20:03:53 +0100

    Upload ftsy team stat views

---
+++ sql/db/ftsy_meister_v.sql
@@ -0,0 +1,68 @@
+create view ftsy_meister_v as 
+
+/* Data from prod table */
+
+select 	`tab`.`season_id` AS `season_id`
+				,`s`.`season_name` AS `season_name`
+				,`tab`.`player_id` AS `player_id`
+				,`tab`.`team_name` AS `team_name`
+				,`tab`.`punkte` AS `punkte`
+				,`tab`.`score_for` AS `score_for`
+				,`tab`.`score_against` AS `score_against`
+				,`tab`.`siege` AS `siege`
+				,`tab`.`niederlagen` AS `niederlagen`
+				,`tab`.`unentschieden` AS `unentschieden`
+				,`tab`.`trost` AS `trost` 
+
+from `ftsy_tabelle_2020` `tab` 
+
+inner join `sm_seasons` `s` 
+	on `tab`.`season_id` = `s`.`season_id`
+
+where `tab`.`spieltag` = 34 and `tab`.`rang` = 1 
+
+union all 
+
+/* Historic data from 2019 */
+
+select 	2019 AS `season_id`
+				,'2019/2020' AS `season_name`
+				,4 AS `player_id`
+				,`tab`.`team` AS `team_name`
+				,`tab`.`punkte` AS `punkte`
+				,`tab`.`score_for` AS `score_for`
+				,`tab`.`score_against` AS `score_against`
+				,`tab`.`siege` AS `siege`
+				,`tab`.`niederlagen` AS `niederlagen`
+				,`tab`.`unentschieden` AS `unentschieden`
+				,`tab`.`trost` AS `trost` 
+
+from `fantasy_tabelle_2019` `tab` 
+
+where `tab`.`spieltag` = 34 and `tab`.`rang` = 1 
+
+union all 
+
+/* Historic data from 2018 */
+
+
+select 	2018 AS `season_id`
+				,'2018/2019' AS `season_name`
+				,`u`.`id` AS `player_id`
+				,coalesce(`u`.`teamname`,`tab`.`team`) AS `team_name`
+				,`tab`.`punkte` AS `punkte`
+				,`tab`.`score_for` AS `score_for`
+				,`tab`.`score_against` AS `score_against`
+				,`tab`.`siege` AS `siege`
+				,`tab`.`niederlagen` AS `niederlagen`
+				,`tab`.`unentschieden` AS `unentschieden`
+				,`tab`.`trost` AS `trost` 
+
+from `fantasy_tabelle_2018` `tab` 
+
+left join `xa7580_db1`.`users` `u` 
+	on `u`.`id` = `tab`.`player`
+
+where `tab`.`spieltag` = 34 and `tab`.`rang` = 1 
+
+order by `season_id` desc
---
+++ sql/db/ftsy_pokalsieger_v.sql
@@ -0,0 +1,23 @@
+create view ftsy_pokalsieger_v as
+
+select 	`s`.`season_id` AS `season_id`
+				,`s`.`season_name` AS `season_name`
+				,`sch`.`buli_round_name` AS `buli_round_name`
+				,`sch`.`ftsy_match_id` AS `ftsy_match_id`
+				,case when `sch`.`ftsy_home_score` > `sch`.`ftsy_away_score` then `sch`.`ftsy_home_id` else `sch`.`ftsy_away_id` end AS `winner_user_id`
+				,case when `sch`.`ftsy_home_score` > `sch`.`ftsy_away_score` then `sch`.`ftsy_home_name` else `sch`.`ftsy_away_name` end AS `winner_team_name`
+				,case when `sch`.`ftsy_home_score` > `sch`.`ftsy_away_score` then `sch`.`ftsy_home_score` else `sch`.`ftsy_away_score` end AS `winner_score`
+				,case when `sch`.`ftsy_home_score` < `sch`.`ftsy_away_score` then `sch`.`ftsy_home_id` else `sch`.`ftsy_away_id` end AS `looser_user_id`
+				,case when `sch`.`ftsy_home_score` < `sch`.`ftsy_away_score` then `sch`.`ftsy_home_name` else `sch`.`ftsy_away_name` end AS `looser_team_name`
+				,case when `sch`.`ftsy_home_score` < `sch`.`ftsy_away_score` then `sch`.`ftsy_home_score` else `sch`.`ftsy_away_score` end AS `looser_score` 
+
+from 	`ftsy_schedule` `sch` 
+
+inner join `xa7580_db1`.`sm_seasons` `s` 
+	on `sch`.`season_id` = `s`.`season_id` 
+
+where 	`sch`.`match_type` = 'cup' 
+				and `sch`.`cup_round` = 'final' 
+				and `sch`.`ftsy_home_score` + `sch`.`ftsy_away_score` > 0 
+
+order by `s`.`season_id` desc
