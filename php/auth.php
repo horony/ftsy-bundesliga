d388dd9ec73a5f94c77794e9e6cb82cae422edc2
Lennart <lhorony@gmail.com>
2023-05-15 21:28:22 +0200

    Changed link to db-connection from relative to absolute

--- php/auth.php
+++ php/auth.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-include '../secrets/mysql_db_connection.php';		
+require('/home/www/dev/secrets/mysql_db_connection.php');		
 mysqli_query( $con, " UPDATE xa7580_db1.users SET last_login = NOW() WHERE username = '".$_SESSION["username"]."' " );
 
 if(!isset($_SESSION["username"])){
-header("Location: ../html/login.php");
+header("Location: /home/www/dev/html/login.php");
 exit(); }
 ?>
