f44789022f15baa877b0fdba85a8a7740e939d6b
Lennart <lhorony@gmail.com>
2023-06-03 16:52:05 +0200

    Modified links from relative to absolute

--- html/navigation.php
+++ html/navigation.php
@@ -1,5 +1,8 @@
+<?php
+//include auth.php file on all secure pages
+include("https://fantasy-bundesliga.de/php/auth.php");
+?>
 <html>
-<?php include('../php/auth.php');?>
 
 <aside>
 
@@ -17,26 +20,26 @@
 
   <!-- The navigation menu -->
   <div class="topnav" id="myTopnav">
-    <a href="../index.php" class="active">Home</a>
+    <a href="https://fantasy-bundesliga.de/index.php" class="active">Home</a>
 
     <div class="dropdown">
       <button class="dropdownbtn">Liga</button>
       <div class="dropdown-content">
-        <a href="spieltag.php">Spieltag</a>
-        <a href="spielstand.php">Tabelle</a>
-        <a href="pokal.php">Pokal</a>
-        <a href="draft.php">Draft</a>
-        <a href="stats.php">Statistiken</a>
+        <a href="https://fantasy-bundesliga.de/html/spieltag.php">Spieltag</a>
+        <a href="https://fantasy-bundesliga.de/html/tabelle.php">Tabelle</a>
+        <a href="https://fantasy-bundesliga.de/html/pokal.php">Pokal</a>
+        <a href="https://fantasy-bundesliga.de/html/draft.php">Draft</a>
+        <a href="https://fantasy-bundesliga.de/html/stats.php">Statistiken</a>
       </div>
     </div>
 
     <div class="dropdown" id="transfermarkt">
       <button class="dropdownbtn" >Transfermarkt</button>
       <div class="dropdown-content">
-        <a href="transfermarkt.php">Spieler aufnehmen</a>
-        <a class="" href="waiver.php">Waiver Priorisierung</a>
-        <a class="" href="delete_waiver.php">Trades & Waivers</a>
-        <a href="research.php?click_player=1018">Spieler-Datenbank</a>
+        <a href="https://fantasy-bundesliga.de/html/transfermarkt.php">Spieler aufnehmen</a>
+        <a class="" href="https://fantasy-bundesliga.de/html/waiver.php">Waiver Priorisierung</a>
+        <a class="" href="https://fantasy-bundesliga.de/html/waiver_delete.php">Trades & Waivers</a>
+        <a href="https://fantasy-bundesliga.de/html/research.php?click_player=1018">Spieler-Datenbank</a>
       </div>
     </div>
 
@@ -45,15 +48,15 @@
       <div class="dropdown-content">
         <?php
           $team_name = strval(mb_convert_encoding($_SESSION["user_teamname"],'UTF-8'));
-          echo "<a href='mein_team.php?show_team='".$team_name."'.php'>Team verwalten</a>";
+          echo "<a href='https://fantasy-bundesliga.de/html/mein_team.php?show_team='".$team_name."'.php'>Team verwalten</a>";
         ?>
-        <a href="../php/redirect-to-active-match.php">Game-Center</a>
+        <a href="https://fantasy-bundesliga.de/php/redirect-to-active-match.php">Game-Center</a>
       </div>
     </div>
 
-    <a href="regelwerk.php" id="regeln">Regeln</a>
+    <a href="https://fantasy-bundesliga.de/html/regelwerk.php" id="regeln">Regeln</a>
 
-    <a href="account_verwaltung.php" id="account">Mein Account</a>
+    <a href="https://fantasy-bundesliga.de/html/account_verwaltung.php" id="account">Mein Account</a>
 
     <a href='javascript:void(0);' class="icon" onclick='responsive_nav()'>&#9776;</a>
 
