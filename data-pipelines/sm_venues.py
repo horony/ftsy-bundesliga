53fd8e1b91d0371502593dcf77a47ddc51e62ff4
Lennart <lhorony@gmail.com>
2023-07-01 15:54:47 +0200

    Changed script to match upgrade from SportMonks 2.0 API to SportMonks 3.0 API

--- data-pipelines/sm_venues.py
+++ data-pipelines/sm_venues.py
@@ -9,9 +9,13 @@ Updates the dimension tables for venues (e.g. Westfalenstadion Dortmund or Allia
 """
 
 import sys
-sys.path.insert(1, './secrets/')
+sys.path.insert(1, '../secrets/')
+from sm_api_connection import sportmonks_token
+
+sys.path.insert(2, '../py/')
+from logging_function import log, log_headline
+
 import requests
-import json
 import pandas as pd 
 import time
 from time import gmtime, strftime
@@ -21,28 +25,39 @@ from sqlalchemy import create_engine
 #   GET META-DATA   #
 #####################
 
-print('\n>>> GET META-DATA <<<\n')
+log_headline('(1/3) GET SEASON DATA FROM API')
+log("Sending query to seasons endpoint")
 
-from sm_api_connection import sportmonks_token
-response = requests.get("https://soccer.sportmonks.com/api/v2.0/leagues?api_token="+sportmonks_token)
-print("API response code: " + str(response.status_code))
+response = requests.get(
+    "https://api.sportmonks.com/v3/football/leagues/82"
+    + "?api_token=" + sportmonks_token
+    + "&include=currentSeason"
+    )
+
+log("API response code: " + str(response.status_code))
+log("Processing season data")
 
 data = response.json()
-league_id = data['data'][0]['id']
-print("Current league-id is: "+ str(league_id))
-season_id = data['data'][0]['current_season_id']
-print("Current season-id is:"+ str(season_id))
-current_round_id = data['data'][0]['current_round_id']
-print("Current round-id is:"+ str(current_round_id))
+current_season_id = data['data']['currentseason']['id']
+
+# search for current round id and get round name
+log("The season_id is " + str(current_season_id))
+
 
 #########################
 #   GET DATA FROM API   #
 #########################
 
-print('\n>>> GET DATA FROM API <<<\n')
+log_headline('(2/3) GET VENUE DATA FROM API')
+log("Sending query to venues endpoint")
 
-response = requests.get("https://soccer.sportmonks.com/api/v2.0/venues/season/"+str(season_id)+"?api_token="+sportmonks_token)
-print("API response code: " + str(response.status_code))
+response = requests.get(
+    "https://api.sportmonks.com/v3/football/venues/seasons/"
+    + str(current_season_id)
+    + "?api_token=" + sportmonks_token)
+
+log("API response code: " + str(response.status_code))
+log("Processing venues data")
 
 data = response.json()
 data = data['data']
@@ -55,9 +70,9 @@ for venue in data:
     venue_list = []
     venue_list.append(venue['id'])
     venue_list.append(venue['name'])
-    venue_list.append(venue['city'])
+    venue_list.append(venue['city_name'])
     venue_list.append(venue['address'])
-    venue_list.append(venue['coordinates'])
+    venue_list.append(str(venue['latitude']) + ',' + str(venue['longitude']))
     venue_list.append(venue['capacity'])
     venue_list.append(venue['surface'])
     venue_list.append(venue['image_path'])
@@ -66,25 +81,28 @@ for venue in data:
 
 # Safe results to DataFrame
 df_venues = pd.DataFrame(columns=['id','name', 'city', 'address', 'coordinates', 'capacity', 'surface', 'image_path', 'load_ts'], data=venue_data)
+log('Found ' + str(df_venues.shape[0]) + ' venues')
 
 ##########################
 #   WRITE INTO DATABASE  #
 ##########################
 
-print('\n>>> WRITE INTO DATABASE <<<\n')
+log_headline('(3/3) WRITE INTO DATABASE')
+log('Connecting to MySQL database')
 
-# Connect to MySQL-database
+# connect to MySQL-database
 from mysql_db_connection import db_user, db_pass, db_port, db_name
 engine = create_engine('mysql+mysqlconnector://'+db_user+':'+db_pass+'@localhost:'+db_port+'/'+db_name, echo=False)  
 
-# Create table if not exists
+# create table if not exists
 try:
     df_venues.to_sql(name='sm_venues', con=engine, index=False, if_exists='fail')
     with engine.connect() as con:
         con.execute('ALTER TABLE `sm_venues` ADD PRIMARY KEY (`id`);')
-    message = 'Table sm_venues created'
+        
+    db_message = 'Table sm_venues created'
 
-# If exists update table through temp table
+# if exists update table through temp table
 except:
     df_venues.to_sql(name='tmp_sm_venues', con=engine, index=False, if_exists='replace')
     with engine.connect() as con:
@@ -92,9 +110,9 @@ except:
         con.execute('INSERT INTO sm_venues SELECT * FROM tmp_sm_venues t2 ON DUPLICATE KEY UPDATE load_ts = t2.load_ts, name = t2.name, city = t2.city, address = t2.address, coordinates = t2.coordinates, capacity = t2.capacity, surface = t2.surface, image_path = t2.image_path;')    
         con.execute('DROP TABLE tmp_sm_venues;')    
 
-    message = "Table sm_venues updated"
+    db_message = "Table sm_venues updated"
 
 finally:
     con.close()
   
-print(message)
+log(db_message)
