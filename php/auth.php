7c22d5a11ff0c769c19c2e56e6f51df8aba7632a
Lennart <lhorony@gmail.com>
2023-07-15 11:14:51 +0200

    Fixed broken link to login.php

--- php/auth.php
+++ php/auth.php
@@ -4,6 +4,6 @@ require('/home/www/secrets/mysql_db_connection.php');
 mysqli_query( $con, " UPDATE xa7580_db1.users SET last_login = NOW() WHERE username = '".$_SESSION["username"]."' " );
 
 if(!isset($_SESSION["username"])){
-header("Location: /home/www/html/login.php");
+header("Location: /html/login.php");
 exit(); }
 ?>
