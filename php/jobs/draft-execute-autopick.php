bca1a0aa1c95dbc669fc11b68bb7453974a392bf
Lennart <81903255+horony@users.noreply.github.com>
2024-08-18 14:56:24 +0200

    Updated for 24/25 season

--- php/jobs/draft-execute-autopick.php
+++ php/jobs/draft-execute-autopick.php
@@ -39,7 +39,7 @@ if (	$draft_meta['update_lock'] == 0 and $draft_meta['draft_status'] == 'running
 
 		WHERE 	pb.ftsy_league_id = 1
 						AND pb.pick is NULL
-						AND scr.fixture_id in (select fixture_id from sm_fixtures where season_id = 19744)
+						AND scr.fixture_id in (select fixture_id from sm_fixtures where season_id = 21795)
 
 		GROUP BY 	pb.id, pb.display_name
 		ORDER BY 	SUM(scr.ftsy_score) desc
